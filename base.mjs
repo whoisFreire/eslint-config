2601f2b86e28cce76eb6506f9c82b15f7b6fdf97
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-29 16:32:19 -0300

    fix: Fix array-bracket-spacing error

--- base.mjs
+++ base.mjs
@@ -1,23 +1,23 @@
 import eslint from '@eslint/js'
-import { neostandard } from 'neostandard/lib/main'
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
 import tseslint from 'typescript-eslint'
 
 export default [
-  ...eslint.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...neostandard({
     ignores: resolveIgnoresFromGitignore(),
   }),
   {
     rules: {
-      '@stylistic/max-len': ['warn', {
+      '@stylistic/max-len': [ 'warn', {
         code: 120,
         tabWidth: 2,
         ignoreUrls: true,
         ignoreComments: false,
-      }],
-      '@stylistic/array-bracket-spacing': 'always',
-      '@stylistic/space-before-function-paren': ['error',
+      } ],
+      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+      '@stylistic/space-before-function-paren': [ 'error',
         {
           anonymous: 'always',
           asyncArrow: 'always',
